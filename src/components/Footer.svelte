6ffafa3edc850a3dc191e1293058d5b6379aa6c2
Jude Russel Cuya <juderusselcuya0421@gmail.com>
2025-08-28 19:48:37 +0800

    Updated UI Components

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,8 +1,8 @@
 <script>
-    import fb from '../lib/assets/fb colored.svg';
-    import ig from '../lib/assets/ig colored.svg';
+    import fb from '../lib/assets/facebook-circled.svg';
+    import ig from '../lib/assets/instagram-circle.svg';
     import github from '../lib/assets/github white.svg';
-    import linkedin from '../lib/assets/linked in.svg';
+    import linkedin from '../lib/assets/linkedin-circled.svg';
 </script>
 
 <div class="bg-black  flex flex-col lg:flex-row lg:justify-between items-center lg:p-30  pt-20 pb-20 text-white w-full">
@@ -13,7 +13,7 @@
     </div>
     <div class="lg:pt-0 pt-3 font-bold lg:justify-end justify-center">
         <p>Jude Russel Barrion Cuya</p>
-        <div class="flex justify-center pt-3 lg:pt-0">
+        <div class="flex justify-center pt-3 lg:pt-1">
             <a href="https://www.facebook.com/JUDERRRUSSEL11121" target="_blank"><img src="{fb}" alt="" class="facebook w-7  transform transition-transform duration-300 hover:scale-120 "></a>
             <a href="https://www.instagram.com/jdrsslc_/?next=%2F" target="_blank"><img src="{ig}" alt="" class="instagram w-7   transform transition-transform duration-300 hover:scale-120"></a>
             <a href="https://github.com/Jude1121" target="_blank"><img src="{github}" alt="" class="github w-7  transform transition-transform duration-300 hover:scale-135"></a>
