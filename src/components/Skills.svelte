d4d8de3c5c86b96b74a3802b845ecbec7872bd33
Jude Russel Cuya <juderusselcuya0421@gmail.com>
2025-08-30 11:44:18 +0800

    Scroll Animation

--- src/components/Skills.svelte
+++ src/components/Skills.svelte
@@ -14,10 +14,10 @@
 </script>
 
 
-<h1 class="lg:pt-30 pt-15 lg:pb-10 pb-7 text-4xl font-extrabold flex justify-center">SKILLS</h1>
+<h1 class="lg:pt-30 pt-15 lg:pb-10 pb-7 text-4xl font-extrabold flex justify-center fadeUp">SKILLS</h1>
 <div class="flex justify-center pt-3 ">
-        <div class="container lg:flex lg:justify-baseline gap-3 w-full justify-center lg:pl-0 lg:pr-0 pl-6 pr-6">
-            <div class="container html lg:justify-baseline flex p-4 bg-white w-full lg:w-103 gap-2 rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
+        <div class=" lg:flex lg:justify-baseline gap-3 w-full justify-center lg:pl-0 lg:pr-0 pl-6 pr-6">
+            <div class="fadeUp html lg:justify-baseline flex p-4 bg-white w-full lg:w-103 gap-2 rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
                 <img src={HTML} alt="" class="w-16 p-1">
                 <div class="">
                     <h1 class="font-bold">HTML</h1>
@@ -32,7 +32,7 @@
                 </div>
             </div>
             <div class="pt-5"></div>
-            <div class="container css justify-baseline flex lg:w-103 p-4 gap-2 w-full bg-white rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
+            <div class="fadeUp css justify-baseline flex lg:w-103 p-4 gap-2 w-full bg-white rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
                  <img src={CSS} alt="" class="w-16 p-1"> 
                  <div>
                     <h1 class="font-bold">CSS</h1>
@@ -52,8 +52,8 @@
 <div class="p-1"></div>
 
 <div class="flex justify-center pt-3 ">
-        <div class="container lg:flex lg:justify-baseline gap-3 w-full justify-center lg:pl-0 lg:pr-0 pl-6 pr-6">
-            <div class="container javaScript lg:justify-baseline flex p-4 bg-white w-full lg:w-103 gap-2 rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
+        <div class="lg:flex lg:justify-baseline gap-3 w-full justify-center lg:pl-0 lg:pr-0 pl-6 pr-6">
+            <div class="fadeUp javaScript lg:justify-baseline flex p-4 bg-white w-full lg:w-103 gap-2 rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
                 <img src={javaScript} alt="" class="w-16 p-1">
                 <div class="">
                     <h1 class="font-bold">JAVASCRIPT</h1>
@@ -68,7 +68,7 @@
                 </div>
             </div>
             <div class="pt-5"></div>
-            <div class="container csharp justify-baseline flex lg:w-103 p-4 gap-2 w-full bg-white rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
+            <div class="fadeUp csharp justify-baseline flex lg:w-103 p-4 gap-2 w-full bg-white rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
                  <img src={cSharp} alt="" class="w-16 p-1"> 
                  <div>
                     <h1 class="font-bold">C#</h1>
@@ -89,7 +89,7 @@
 
 <div class="flex justify-center pt-3 ">
         <div class="lg:flex lg:justify-baseline gap-3 w-full justify-center lg:pl-0 lg:pr-0 pl-6 pr-6">
-            <div class="container java lg:justify-baseline flex p-4 bg-white w-full lg:w-103 gap-2 rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
+            <div class="fadeUp java lg:justify-baseline flex p-4 bg-white w-full lg:w-103 gap-2 rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
                 <a href="https://www.java.com/en/" target="_blank"><img src={JAVA} alt="" class="w-16 p-1"></a>
                 <div class="">
                     <h1 class="font-bold">JAVA</h1>
@@ -104,7 +104,7 @@
                 </div>
             </div>
             <div class="pt-5"></div>
-            <div class="container python justify-baseline flex lg:w-103 p-4 gap-2 w-full bg-white rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
+            <div class="fadeUp python justify-baseline flex lg:w-103 p-4 gap-2 w-full bg-white rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
                  
                 <a href="https://www.python.org/" target="_blank"><img src={PYTHON} alt="" class="w-16 p-1"> </a>
                  <div>
@@ -126,7 +126,7 @@
 
 <div class=" flex justify-center pt-3 ">
         <div class="lg:flex lg:justify-baseline gap-3 w-full justify-center lg:pl-0 lg:pr-0 pl-6 pr-6">
-            <div class="container tailwind lg:justify-baseline flex p-4 bg-white w-full lg:w-103 gap-2 rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
+            <div class="fadeUp tailwind lg:justify-baseline flex p-4 bg-white w-full lg:w-103 gap-2 rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
                 <a href="https://tailwindcss.com/" target="_blank"><img src={Tailwind} alt="" class="w-16 p-1"></a>
                 <div class="">
                     <h1 class="font-bold">TAILWIND</h1>
@@ -141,7 +141,7 @@
                 </div>
             </div>
             <div class="pt-5"></div>
-            <div class="container svelte justify-baseline flex lg:w-103 p-4 gap-2 w-full bg-white rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
+            <div class="fadeUp svelte justify-baseline flex lg:w-103 p-4 gap-2 w-full bg-white rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
                  <a href="https://svelte.dev/" target="_blank"><img src={Svelte} alt="" class="w-16 p-1"></a>
                  <div>
                     <h1 class="font-bold">SVELTE</h1>
@@ -162,7 +162,7 @@
 
 <div class=" flex justify-center pt-3 lg:pb-30 pb-15">
         <div class="lg:flex lg:justify-baseline gap-3 w-full justify-center lg:pl-0 lg:pr-0 pl-6 pr-6">
-            <div class="container figma lg:justify-baseline flex p-4 bg-white w-full lg:w-103 gap-2 rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
+            <div class="fadeUp figma lg:justify-baseline flex p-4 bg-white w-full lg:w-103 gap-2 rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
                 <a href="https://www.figma.com/" target="_blank"><img src={Figma} alt="" class="w-16 p-1"></a>
                 <div class="">
                     <h1 class="font-bold">FIGMA</h1>
@@ -177,7 +177,7 @@
                 </div>
             </div>
             <div class="pt-5"></div>
-            <div class="container svelte justify-baseline flex lg:w-103 p-4 gap-2 w-full bg-white rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
+            <div class="fadeUp svelte justify-baseline flex lg:w-103 p-4 gap-2 w-full bg-white rounded-2xl shadow-lg transform transition-transform duration-300 hover:scale-110">
                  <a href="https://www.adobe.com/ph_en/products/photoshop.html" target="_blank"><img src={Photoshop} alt="" class="w-16 p-1"></a>
                  <div>
                     <h1 class="font-bold">ADOBE PHOTOSHOP</h1>
