ab444454e310894715d787a3e0c1bc8ae6fb82d7
Jude Russel Cuya <juderusselcuya0421@gmail.com>
2026-03-04 08:45:23 +0800

    Project name change

--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -11,8 +11,8 @@
              <a href="https://www.figma.com/design/Q9fo4GNksmNTh8xtzMbtNg/Villa-Salome-Proto?node-id=0-1&p=f&t=iYNiGhtYbh48EfOZ-0" target="_blank"><img class="border-0 hover:border-1 rounded-4xl hover:rounded-none" src="{VillaSalome}" alt=""></a>
             </div>
             <div class="container lg:w-190  pt-10">
-                <h1 class="container lg:pt-20 sm:pl-5 text-xl  font-bold flex justify-center">
-                        VILLA SALOME HOTEL AND RESORT ONLINE MANANGEMENT SYSTEM - UI/UX FIGMA PROTOTYPE               
+                <h1 class="container lg:pt-20 sm:pl-5 text-xl  font-bold flex justify-baseline">
+                        VILLA SALOME HOTEL AND RESORT - UI/UX FIGMA PROTOTYPE               
                 </h1>
                 <p class="container lg:pt-5 sm:pl-5  pt-5 pb-8">
                     Our project involves developing a Web-Based Online Reservation Management System for Villa Salome Resort to replace traditional booking methods with a modern, efficient online platform. This system will allow guests to conveniently and accurately reserve activities and accommodations in advance, while ensuring alignment with the resort’s specific requirements. By automating the reservation process, it will reduce the workload of staff and significantly improve the speed and efficiency of managing bookings and related operations.
